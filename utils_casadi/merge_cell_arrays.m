7dd2f15179350161a1e9381f43e86b7eb3e99795
Emanuel <emanuel.huber@student.tuwien.ac.at>
2024-03-29 19:03:31 +0100

    updated helper function for easier usage

--- utils_casadi/merge_cell_arrays.m
+++ utils_casadi/merge_cell_arrays.m
@@ -1,4 +1,8 @@
-function merged_cell = merge_cell_arrays(input_cells)
+function merged_cell = merge_cell_arrays(input_cells, return_type)
+arguments
+  input_cells = {};
+  return_type = "cell"; % default SX datatype
+end
 % Function to merge a cell array into a single row vector
 % Description:
 %   This function takes a cell array (`input_cells`) containing elements
@@ -8,6 +12,7 @@ function merged_cell = merge_cell_arrays(input_cells)
 %
 % Inputs:
 %   input_cells: A cell array containing elements of any size.
+%   return_type: returns merged cell as 1d cell or 1d vector.
 %
 % Outputs:
 %   merged_cell: A 1xn cell array where n is the sum of all elements
@@ -17,5 +22,11 @@ function merged_cell = merge_cell_arrays(input_cells)
   temp = cellfun(@(x) x(:)', input_cells, 'UniformOutput', false);
 
   % Concatenate all column vectors into a single cell array
-  merged_cell = {[temp{:}]};
+  if(strcmp(return_type, "cell"))
+    merged_cell = {[temp{:}]};
+  elseif(strcmp(return_type, "vector"))
+    merged_cell = [temp{:}];
+  else
+    error('return type have to be "cell" or "vector"!');
+  end
 end
