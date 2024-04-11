30bfa240f6b9b75bab0f87ee7bc94699b51a977d
Emanuel <emanuel.huber@student.tuwien.ac.at>
2024-04-11 13:53:58 +0200

    Updated YAML Files and added simulation for frictionless pendulum:
    - New Calculation of inertia: Movements seems realistic but are they correct? Cannot be compared with simulink simulations, which was in casadi and simulink 1:1 the same

--- urdf_creation/2dof_sys_create_urdf.py
+++ urdf_creation/2dof_sys_create_urdf.py
@@ -1,5 +1,15 @@
 from odio_urdf import *
 import yaml
+import matplotlib.pyplot as plt
+
+import pinocchio as pin
+import numpy as np
+import sys
+import os
+from os.path import dirname, join, abspath
+import time
+ 
+from pinocchio.visualize import MeshcatVisualizer
 
 # MUJOCO Conversion
 # /media/daten/Anwendungen/mujoco-2.3.7/bin/compile /media/daten/Projekte/Studium/Master/Masterarbeit_SS2024/2DOF_Manipulator/MA24_simulink/urdf_creation/2dof_sys.urdf /media/daten/Projekte/Studium/Master/Masterarbeit_SS2024/2DOF_Manipulator/MA24_simulink/urdf_creation/2dof_sys.xml
@@ -34,12 +44,12 @@ def link(link_name, link_dict, color, stl_name):
     """
     ret = Link(
         Inertial(
-            Origin(link_dict['d__inertiaOrigin']),
+            Origin(link_dict['d__link']),
             Mass(value=link_dict['m']),
             inertia_fun(link_dict['d__inertiaMatrixDiag'])   # Inertia([inertia_dict(link_dict['d__inertiaMatrixDiag'])])),
             ),
         Visual(
-            Origin(link_dict['d__joint']),
+            Origin(link_dict['d__link']),
             Geometry(Mesh(filename = filepath+stl_name, scale="0.001 0.001 0.001")),
             color_fun(color)),
         #Collision(
@@ -58,38 +68,25 @@ def joint(joint_name, parent_name, child_name, link_dict, type):
     ret = Joint(
         Parent(link=parent_name),
         Child(link=child_name),
-        Origin(link_dict['d__inertiaOrigin']),
+        Origin(link_dict['d__joint']),
         Axis(xyz=link_dict['axis__joint']),
         Limit(lower=link_dict['limits__joint'][0], upper=link_dict['limits__joint'][1], effort=100, velocity=10**8),
-        Safety_controller(soft_lower_limit=link_dict['limits__joint'][0],soft_upper_limit=link_dict['limits__joint'][1],
-            k_position=0, k_velocity=10**8),
-        Dynamics(damping=1),
+        #Safety_controller(soft_lower_limit=link_dict['limits__joint'][0],soft_upper_limit=link_dict['limits__joint'][1],
+        #    k_position=0, k_velocity=10**8),
+        Dynamics(damping=0, friction=0),
         type=type,
         name= joint_name)
-    return ret
-
-def transmission(transmission_name, joint_name, motor_name, hw_interface='PositionJointInterface'):
-    """ Definition of one transmission """
-    ret = Transmission(
-        transmission_name,
-        Type(xmltext="transmission_interface/SimpleTransmission"),
-        Transjoint(joint_name, Hardwareinterface(xmltext = hw_interface)),
-        Actuator(motor_name, 
-            Hardwareinterface(xmltext = hw_interface),
-            Mechanicalreduction(xmltext = "1")
-        )
-    )
     return ret
 
 my_robot = Robot('2dof_sys',
     #Link(name='world'),
     link('world', data['holder'], 'Grey', 'planar_manipulator-holder_fin.stl'),
     joint('joint1', 'world', 'link1', data['holder'], "revolute"),
-    transmission('joint1_transmission', 'joint1', 'joint1_actuator'),
     link('link1', data['link1'], 'Orange', 'planar_manipulator-bulk1_fin.stl'),
-    joint('joint2', 'link1', 'link2', data['holder'], "revolute"),
-    transmission('joint2_transmission', 'joint2', 'joint2_actuator'),
-    link('link2', data['link2'], 'Orange', 'planar_manipulator-bulk2_fin.stl')
+    joint('joint2', 'link1', 'link2', data['link1'], "revolute"),
+    link('link2', data['link2'], 'Orange', 'planar_manipulator-bulk2_fin.stl'),
+    joint('jointTCP', 'link2', 'TCP', data['link2'], "fixed"),
+    Link(name='TCP')
 ) 
 
 print(my_robot) #Dump urdf to stdout
@@ -110,46 +107,126 @@ with open("./urdf_creation/2dof_sys.urdf", "w+") as f:
     f.write(str_my_robot)
     f.close()
 
+# create mujoco file
+os.system('rm /media/daten/Projekte/Studium/Master/Masterarbeit_SS2024/2DOF_Manipulator/MA24_simulink/urdf_creation/2dof_sys.xml')
+os.system('/media/daten/Anwendungen/mujoco-2.3.7/bin/compile /media/daten/Projekte/Studium/Master/Masterarbeit_SS2024/2DOF_Manipulator/MA24_simulink/urdf_creation/2dof_sys.urdf /media/daten/Projekte/Studium/Master/Masterarbeit_SS2024/2DOF_Manipulator/MA24_simulink/urdf_creation/2dof_sys.xml')
 
-#robot = URDF.load('./urdf_creation/2dof_sys.urdf')
-#robot.show(joint1=0, joint2=0)
+# add actuator to file:
+actuator_string = '''
+<actuator>
+    <position name="q1" joint="joint1" ctrlrange="-3.141 3.141"/>
+    <position name="q2" joint="joint2" ctrlrange="-3.141 3.141"/>
+</actuator>\n
+'''
 
-import pinocchio as pin
-import numpy as np
-import sys
-import os
-from os.path import dirname, join, abspath
-import time
+with open("./urdf_creation/2dof_sys.xml", "r+") as f:
+    text = f.read()
+    text_new= text[:-10] + actuator_string + text[-10:]
+    f.seek(0)
+    f.write(text_new)
+    f.close()
+
+
+run_mujoco=False
+if run_mujoco:
+    # run mujoco
+    os.system('/media/daten/Anwendungen/mujoco-2.3.7/bin/simulate /media/daten/Projekte/Studium/Master/Masterarbeit_SS2024/2DOF_Manipulator/MA24_simulink/urdf_creation/scene_2dof.xml')
+
+plot_true=True
+if plot_true:
+    # Load the URDF model.
+    # Conversion with str seems to be necessary when executing this file with ipython
+    mesh_dir = '/media/daten/Projekte/Studium/Master/Masterarbeit_SS2024/2DOF_Manipulator/MA24_simulink/stl_files/'
+    urdf_model_path  = '/media/daten/Projekte/Studium/Master/Masterarbeit_SS2024/2DOF_Manipulator/MA24_simulink/urdf_creation/2dof_sys.urdf'
+    #urdf_model_path  = '/media/daten/Projekte/Studium/Master/Masterarbeit_SS2024/2DOF_Manipulator/MA24_simulink/urdf_creation/pendulum.urdf'
+    # https://gepettoweb.laas.fr/doc/stack-of-tasks/pinocchio/master/doxygen-html/
+    
+    model, collision_model, visual_model = pin.buildModelsFromUrdf(urdf_model_path, mesh_dir)
+
+    viz = MeshcatVisualizer(model, collision_model, visual_model)
+
+    try:
+        viz.initViewer(open=True)
+    except ImportError as err:
+        print(
+            "Error while initializing the viewer. It seems you should install Python meshcat"
+        )
+        print(err)
+        sys.exit(0)
+
+    viz.loadViewerModel()
+
+    # Display a robot configuration.
+    q0 = pin.neutral(model)
+    viz.display(q0)
+    viz.displayVisuals(True)
+    viz.display(pin.randomConfiguration(model))
+
+    q1 = np.array([np.pi/2, np.pi/2])
+    q1 = q0
+    v0 = np.random.randn(model.nv) * 2 # Anfangsgeschwindigkeit
+    v0 = v0*0
+
+    data = viz.data
+    pin.forwardKinematics(model, data, q1, v0)
+
+    frame_id = model.getFrameId('TCP')
+    viz.display()
+
+    viz.drawFrameVelocities(frame_id=frame_id)
+
+    model.gravity.linear[:] = [0, -9.81, 0]
+    dt = 0.001
+
+    def sim_loop():
+        tau0 = np.zeros(model.nv)#-0.001 # input torque
+        qs = [q1]
+        vs = [v0]
+        nsteps = 10000
+        for i in range(nsteps):
+            q = qs[i]
+            v = vs[i]
+            a1 = pin.aba(model, data, q, v, tau0)
+            vnext = v + dt * a1
+            qnext = pin.integrate(model, q, dt * vnext)
+            qs.append(qnext)
+            vs.append(vnext)
+            viz.display(qnext)
+            viz.drawFrameVelocities(frame_id=frame_id)
+        return qs, vs
  
-from pinocchio.visualize import MeshcatVisualizer
- 
-# Load the URDF model.
-# Conversion with str seems to be necessary when executing this file with ipython
-mesh_dir = '/media/daten/Projekte/Studium/Master/Masterarbeit_SS2024/2DOF_Manipulator/MA24_simulink/stl_files/'
-urdf_model_path  = '/media/daten/Projekte/Studium/Master/Masterarbeit_SS2024/2DOF_Manipulator/MA24_simulink/urdf_creation/2dof_sys2.urdf'
-#urdf_model_path  = '/media/daten/Projekte/Studium/Master/Masterarbeit_SS2024/2DOF_Manipulator/MA24_simulink/urdf_creation/pendulum.urdf'
- 
-model, collision_model, visual_model = pin.buildModelsFromUrdf(
-    urdf_model_path, mesh_dir, pin.JointModelFreeFlyer()
-)
- 
-viz = MeshcatVisualizer(model, collision_model, visual_model)
+    qs, vs = sim_loop()
 
-try:
-    viz.initViewer(open=True)
-except ImportError as err:
-    print(
-        "Error while initializing the viewer. It seems you should install Python meshcat"
-    )
-    print(err)
-    sys.exit(0)
+    def my_callback(i, *args):
+        viz.drawFrameVelocities(frame_id)
+    
+    # Show all robot methods
+    #for name, function in model.__class__.__dict__.items():
+    #    print(' **** %s: %s' % (name, function.__doc__))
 
-viz.loadViewerModel()
+    plot_data = False
+    if plot_data:
+        qs_arr = np.array(qs)
+        ts = np.linspace(0, 10000*dt, 10000 + 1)
 
-# Display a robot configuration.
-q0 = pin.neutral(model)
-viz.display(q0)
-viz.displayVisuals(True)
+        plt.style.use('dark_background')
+        plt.figure(figsize=(10, 5))
 
-while True:
-    time.sleep(1.0)
+        # Plot the joint configurations qs over time ts
+        plt.plot(ts, qs_arr[:, 0], label='q1', color='y', linewidth=1)
+        plt.plot(ts, qs_arr[:, 1], label='q2', color='b', linewidth=1)
+
+        # Set the title and labels
+        plt.title('Joint Configurations qs over Time ts')
+        plt.xlabel('Time (s)')
+        plt.ylabel('Joint Configurations')
+
+        # Show the legend
+        plt.legend()
+
+        # Show the plot
+        plt.show()
+
+    while True:
+        time.sleep(1.0)
+        viz.play(qs, dt, callback=my_callback)
--- urdf_creation/create_yaml_files_from_robot_param.m
+++ urdf_creation/create_yaml_files_from_robot_param.m
@@ -1,12 +1,11 @@
 clc;clear;
 
 parameters;
-
-addpath(genpath('./urdf_creation/'));
 import yaml.*;
 
 param_robot.holder.R__joint = eye(3);
-param_robot.holder.d__joint = [0 0 0];
+param_robot.holder.d__joint = [0 param_robot.l__I 0];
+param_robot.holder.d__link = [0 0 0];
 param_robot.holder.axis__joint = [0 0 1];
 param_robot.holder.limits__joint = [-pi pi];
 param_robot.holder.R__inertiaOrigin = eye(3);
@@ -15,7 +14,8 @@ param_robot.holder.d__inertiaMatrixDiag = param_robot.inertia_matrix_sI;
 param_robot.holder.m = param_robot.mI;
 
 param_robot.link1.R__joint = eye(3);
-param_robot.link1.d__joint = [param_robot.l__s1 param_robot.l__I 0];
+param_robot.link1.d__joint = [param_robot.l__1 0 0];
+param_robot.link1.d__link = [param_robot.l__s1 0 0];
 param_robot.link1.axis__joint = [0 0 1];
 param_robot.link1.limits__joint = [-pi pi];
 param_robot.link1.R__inertiaOrigin = eye(3);
@@ -24,7 +24,10 @@ param_robot.link1.d__inertiaMatrixDiag = param_robot.inertia_matrix_s1;
 param_robot.link1.m = param_robot.m1;
 
 param_robot.link2.R__joint = eye(3);
-param_robot.link2.d__joint = [param_robot.l__1+param_robot.l__s2 param_robot.l__I 0];
+param_robot.link2.d__joint = [param_robot.l__2 0 0];
+param_robot.link2.d__link = [param_robot.l__s2 0 0];
+param_robot.link2.axis__joint = [0 0 1];
+param_robot.link2.limits__joint = [0 0];
 param_robot.link2.R__inertiaOrigin = eye(3);
 param_robot.link2.d__inertiaOrigin = [0 0 0];
 param_robot.link2.d__inertiaMatrixDiag = param_robot.inertia_matrix_s2;
