194cd90dbdf3d2ea6064fec6f240526d8ce0c3a8
Emanuel <emanuel.huber@student.tuwien.ac.at>
2024-05-03 07:46:04 +0200

    updated param_visual for 7dof

--- utils/matlab_init_7dof/param_visual.m
+++ utils/matlab_init_7dof/param_visual.m
@@ -25,45 +25,8 @@ coord_yaxis_opacity = 1;
 coord_zaxis_color = [0 0.0 1]; % blue
 coord_zaxis_opacity = 1;
 
-% Transparenzfaktor für achsen und stl files (damit man KOS sieht)
-transparency_base = 1/2;
-
 %% Robot gravity
-param_robot.g_vis=[0;-param_robot.g;0];
-
-%% Simscape Settings
-param_robot.R_0 = eye(3);
-param_robot.p_0 = [0 0 0];
-
-param_robot.holder.R_0 = eye(3);
-param_robot.holder.d_0 = [0 param_robot.l__I 0];
-param_robot.holder.R__joint = param_robot.holder.R_0;
-param_robot.holder.d__joint = param_robot.holder.d_0;
-param_robot.holder.R__inertiaOrigin = eye(3);
-param_robot.holder.d__inertiaOrigin = [0 0 0];
-param_robot.holder.d__inertiaMatrixDiag = param_robot.inertia_matrix_sI;
-param_robot.holder.m = param_robot.mI;
-
-param_robot.link1_offset = [param_robot.l__s1 0 0];
-
-param_robot.link1.R__joint = eye(3);
-param_robot.link1.d__joint = [param_robot.l__1-param_robot.l__s1 0 0];
-param_robot.link1.R__inertiaOrigin = eye(3);
-param_robot.link1.d__inertiaOrigin = [0 0 0];
-param_robot.link1.d__inertiaMatrixDiag = param_robot.inertia_matrix_s1;
-param_robot.link1.m = param_robot.m1;
-
-param_robot.link2_offset = [param_robot.l__s2 0 0];
-
-param_robot.link2.R__joint = eye(3);
-param_robot.link2.d__joint = [param_robot.l__2 - param_robot.l__s2 0 0];
-param_robot.link2.R__inertiaOrigin = eye(3);
-param_robot.link2.d__inertiaOrigin = [0 0 0];
-param_robot.link2.d__inertiaMatrixDiag = param_robot.inertia_matrix_s2;
-param_robot.link2.m = param_robot.m2;
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% FR3 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+param_robot.g_vis=[0;0;-param_robot.g];
 
 param_robot_fr3 = struct;
 white = [1 1 1];
@@ -74,12 +37,6 @@ light_blue = [0.039216 0.541176 0.780392];
 
 fr3_base_opacity = 1;
 rpy2rotm = @(rpy) eul2rotm(rpy, "XYZ");
-
-%%%% Param Robot (FR3) %%%% (TODO)
-%param_robot.l__I = 0.0583789; %m
-%param_robot.mI = rho*249312.22389582483*1e-9; %kg
-%param_robot.inertia_matrix_sI = rho*[2.60656e+08 4.65146e+08 4.47456e+08]*1e-15; %kgm^2
-%param_robot.II = param_robot.inertia_matrix_sI(3); %kgm^2
 
 %% FR Robot
 fr3.robot = struct;
