4c7f55208baf424b74726346eac07bfae46e71e1
Emanuel Huber <emanuel.huber@student.tuwien.ac.at>
2024-05-31 19:06:17 +0200

    Added python2casadi with pinocchio 3xx implementation. Delivers nearly the same as darli (see https://github.com/Emucde/MA24_simulink/issues/58 and https://github.com/Emucde/MA24_simulink/issues/63).

--- urdf_creation/7dof_sys_pinocchio3x_preview_to_casadi.py
+++ urdf_creation/7dof_sys_pinocchio3x_preview_to_casadi.py
@@ -6,6 +6,24 @@ import casadi as cs
 import pinocchio as pin
 from pinocchio import casadi as cpin
 
+def SX00_to_SX0(J_fun, q, q_p=None):
+    if q_p is None:
+        J_val = J_fun(q)
+    else:
+        J_val = J_fun(q, q_p)
+
+    for row in range(J_val.shape[0]):
+        for col in range(J_val.shape[1]):
+            try:
+                if(J_val[row, col] == 0):
+                    J_val[row, col] = 0
+            except:
+                pass
+    if q_p is None:
+        return cs.Function(J_fun.name(), [q], [J_val], J_fun.name_in(), J_fun.name_out())
+    else:
+        return cs.Function(J_fun.name(), [q, q_p], [J_val], J_fun.name_in(), J_fun.name_out())
+
 mesh_dir = os.path.join(os.path.dirname(__file__), '..', 'stl_files')
 urdf_path = os.path.join(os.path.dirname(__file__), 'fr3.urdf')
 
@@ -32,6 +50,12 @@ cpin.framesForwardKinematics(casadi_model, cdata, q)
 endEffector_ID = casadi_model.getFrameId(end_link)
 
 f_e = cs.Function('f_e', [q], [cdata.oMf[endEffector_ID].translation], ['q'], ['f_e'])
+H_SX = cdata.oMf[endEffector_ID].homogeneous
+H = cs.Function('H', [q], [H_SX], ['q'], ['H(q)'])
+
+quat7dim_SX = cpin.SE3ToXYZQUAT(cdata.oMf[endEffector_ID])
+quat_SX = cs.vertcat(quat7dim_SX[6], quat7dim_SX[3:6])
+quat = cs.Function('quat', [q], [quat_SX], ['q'], ['quat(q)'])
 
 cpin.forwardKinematics(casadi_model, cdata, q, q_p, q_pp)
 # cpin.forwardDynamics(casadi_model, cdata, q, q_p, u)
@@ -48,68 +72,43 @@ g = cs.Function('g', [q], [g_SX], ['q'], ['g(q)'])
 M_SX = cpin.crba(casadi_model, cdata, q)
 M = cs.Function('M', [q], [M_SX], ['q'], ['M(q)']) # inertia matrix
 
-joint_names = casadi_model.joint_names  # names of the joints
-print(joint_names)
+tau_SX = cpin.rnea(casadi_model, cdata, q, q_p, q_pp) # INV DYN: tau = M(q)q_pp + C(q, q_p)q_p + g(q) = M(q)q_pp + C_rnea(q, q_p)
+inv_dyn_tau = cs.Function('inv_dyn', [q, q_p, q_pp], [tau_SX], ['q', 'q_p', 'q_pp'], ['tau(q, q_p, q_pp)'])
 
-
-print('fin')
-M_SX = casadi_model.inertia()
-g_SX = casadi_model.gravity()
-#bias_force = casadi_model.bias_force
-#C_SX = casadi_model.coriolis_matrix() # not implemented!
-C_rnea_SX = - casadi_model.coriolis() + g_SX # Nur so stimmt es mit Maple überein!
-
-casadi_model.add_body([end_link])
-# casadi_model.bodies # show models
-J_SX = casadi_model.body(end_link).jacobian.world_aligned
-J = cs.Function('J', [q], [J_SX], ['q'], ['J(q)'])
-
-# Leider ist d/dt J völlig falsch...
-#J_p_SX = casadi_model.body(end_link).jacobian_dt.world_aligned
-#J_p = cs.Function('J_p', [q, q_p], [J_p_SX], ['q', 'q_p'], ['J_p(q, q_p)'])
-J_p = cs.Function('J_p', [q, q_p], [cs.reshape(  cs.jacobian(J(q), q) @ q_p, 6 ,n)], ['q', 'q_p'], ['J_p(q, q_p)']) # derivative of geometric jacobian
-J_p = SX00_to_SX0(J_p, q, q_p)
-
-p_e_SX = casadi_model.body(end_link).position
-R_e_SX = casadi_model.body(end_link).rotation
-H = cs.Function('H', [q], [cdata.oMf[endEffector_ID].homogeneous], ['q'], ['H(q)']) # homogenious transformation matrix
-
-quat_q4123_SX = casadi_model.body(end_link).quaternion
-quat_e = cs.Function('quat_e', [q], [cs.vertcat(quat_q4123_SX[3], quat_q4123_SX[0:3])], ['q'], ['quat_e(q)']) # order is quat_e(q) = [q1, q2, q3, q4]
-
-q_0 = [0, 0, np.pi/4, -np.pi/2, 0, np.pi/2, 0]
-
-# Achtung der Gravitiy Vektor schaut default mäßig in [0, 0, -9.81]. Eigentlich sollte das Passen, aber scheinbar kommt es zu einen
-# Vorzeichenfehler im Vergleich zu den Maple berechnungen, in denen ich den Gravity Vektor ebenfalls auf [0, 0, -9.81] gesetzt habe.
-inv_dyn_tau = cs.Function('inv_dyn', [q, q_p, q_pp], [casadi_model.inverse_dynamics()], ['q', 'q_p', 'q_pp'], ['tau(q, q_p, q_pp)'])
-
-C_rnea = cs.Function('C_rnea', [q, q_p], [C_rnea_SX], ['q', 'q_p'], ['C_rnea(q, q_p) = C(q, q_p)q_p + g(q)']) # coriolis matrix
-
-#g = cs.Function('g', [q], [inv_dyn_tau(q, cs.SX(n,1), cs.SX(n,1))], ['q'], ['g(q)'])
-g = cs.Function('g', [q], [g_SX], ['q'], ['g(q)'])
-
-M = cs.Function('M', [q], [M_SX], ['q'], ['M(q)']) # inertia matrix
+C_rnea_SX = cpin.rnea(casadi_model, cdata, q, q_p, cs.SX(n,1))# - g_SX # Nur so stimmt es mit Maple überein!
+C_rnea = cs.Function('C_rnea', [q, q_p], [C_rnea_SX], ['q', 'q_p'], ['C_rnea(q, q_p) = C(q, q_p)q_p + g(q)']) # = n(q, q_p) = C(q, q_p)q_p + g(q)
+C_rnea_v2 = cs.Function('C_rnea_v2', [q, q_p], [C(q, q_p)@q_p + g(q)], ['q', 'q_p'], ['C_rnea(q, q_p) = C(q, q_p)q_p + g(q)']) # = n(q, q_p) = C(q, q_p)q_p + g(q)
 
 M_mat = cs.SX(n,n)
-
 for i in range(n):
     q_pp_vec = cs.SX(n,1)
     q_pp_vec[i] = 1
     M_mat[:, i] = inv_dyn_tau(q, cs.SX(n,1), q_pp_vec) - g(q) # methode nach ott
-M_v2 = cs.Function('M', [q], [M_mat], ['q'], ['M(q)']) # inertia matrix
+M_v2 = cs.Function('M', [q], [cs.simplify(M_mat)], ['q'], ['M(q)']) # inertia matrix
 
-# tic = time.time()
-# for _ in range(10000):
-#     M_v2(q_0)
-# toc = time.time()
-# runtime = toc - tic
-# print(runtime)
+#tic = time.time()
+#for _ in range(10000): 
+#    #M_v2(q_0)
+#    C_rnea(q_0, q_0) # C_rnea: 0.22s, C_rnea_v2 (cbra): 0.39s
+#toc = time.time()
+#runtime = toc - tic
+#print(runtime)
+# Wieso ist M(q) mit cbra schneller als M_v2(q) mit rnea? (M: 0.177s, M_v2: 0.288s)
 
 
-q_pp_aba_SX = casadi_model.forward_dynamics()
 
-print(sys_fun_qpp(q_0, q_0, q_0))
-print([-21.0914, -2.73093, 18.9557, 12.8435, 9.98295, 44.4745, 9.12496]) # aus Maple
+J_SX = cpin.computeFrameJacobian(casadi_model, cdata, q, endEffector_ID, cpin.ReferenceFrame.LOCAL_WORLD_ALIGNED)
+J = cs.Function('J', [q], [J_SX], ['q'], ['J(q)'])
+
+# alles irgwas
+#J_p_SX = cpin.computeFrameJacobianTimeVariation(casadi_model, cdata, q, q_p, endEffector_ID, cpin.ReferenceFrame.LOCAL_WORLD_ALIGNED)
+#J_p_SX = cpin.getFrameJacobianTimeVariation(casadi_model, cdata, endEffector_ID, cpin.ReferenceFrame.LOCAL_WORLD_ALIGNED)
+#J_p_SX = cpin.frameJacobianTimeVariation(casadi_model, cdata, q, q_p, endEffector_ID, cpin.ReferenceFrame.LOCAL_WORLD_ALIGNED)
+
+J_p_SX = cs.reshape(  cs.jacobian(J(q), q) @ q_p, 6 ,n)
+J_p = cs.Function('J_p', [q, q_p], [J_p_SX], ['q', 'q_p'], ['J_p(q, q_p)'])
+J_p = SX00_to_SX0(J_p, q, q_p)
+
 
 sys_fun_x = cs.Function('sys_fun_x', [x, u], [cs.vertcat(x[n:2*n], sys_fun_qpp(x[0:n], x[n:2*n], u))], ['x', 'u'], ['d/dt x = f(x, u)'])
 
@@ -119,7 +118,7 @@ M.save('./s_functions/s_functions_7dof/inertia_matrix_py.casadi')
 C_rnea.save('./s_functions/s_functions_7dof/n_q_coriols_qp_plus_g_py.casadi')
 g.save('./s_functions/s_functions_7dof/gravitational_forces_py.casadi')
 H.save('./s_functions/s_functions_7dof/hom_transform_endeffector_py.casadi')
-quat_e.save('./s_functions/s_functions_7dof/quat_endeffector_py.casadi')
+quat.save('./s_functions/s_functions_7dof/quat_endeffector_py.casadi')
 J.save('./s_functions/s_functions_7dof/geo_jacobian_endeffector_py.casadi')
 J_p.save('./s_functions/s_functions_7dof/geo_jacobian_endeffector_p_py.casadi')
 
