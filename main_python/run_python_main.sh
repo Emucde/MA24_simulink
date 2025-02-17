0ccf16d599859f407ca4a15021fe3609bf5e3441
Emanuel Huber <emanuel.huber@student.tuwien.ac.at>
2025-02-17 15:28:39 +0100

    updated cmakelist file and run files

--- main_python/run_python_main.sh
+++ main_python/run_python_main.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-source ~/.bashrc
-PYTHONPATH=""
-source $CONDA_PREFIX/etc/profile.d/conda.sh
-conda activate mpc
+
+if [ ! -d "/opt/ros/humble" ]; then
+	eval "$(conda shell.bash hook)"
+	conda activate mpc
+else
+	source ~/.bashrc
+	PYTHONPATH=""
+	source $CONDA_PREFIX/etc/profile.d/conda.sh
+	conda activate mpc
+fi
 
 # current_dir=$(pwd)
 # cd $masterdir
